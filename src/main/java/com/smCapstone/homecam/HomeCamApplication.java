a4467c39ae0b68afc16139b79a3bb24a527ce858
tuna1025 <jo215098@gmail.com>
2026-04-02 22:55:50 +0900

    ✨feat: 프로젝트 초기 환경 세팅 및 공통 기능 추가
    - Swagger API 문서화 설정 추가
    - JPA Auditing 기반 BaseEntity 추가
    - 전역 예외 처리를 위한 GlobalExceptionAdvice 및 ApiResponse 구현

---
+++ src/main/java/com/smCapstone/homecam/HomeCamApplication.java
@@ -0,0 +1,15 @@
+package com.smCapstone.homecam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class HomeCamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeCamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smCapstone/homecam/HomeCamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smCapstone.homecam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
